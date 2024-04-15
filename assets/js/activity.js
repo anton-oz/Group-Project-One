d4d2bb3a43538192c0e37fe6aaaf2efd80e965c7
Jacqueline Obeidzinski <jobeidzinski2@gmail.com>
2024-04-15 14:44:21 -0500

    adding weather to tab

--- assets/js/activity.js
+++ assets/js/activity.js
@@ -28,11 +28,28 @@ fetch (weatherURL)
         console.log(weather);
         feelsLike = localStorage.setItem("feelsLike", weather.current_condition[0].FeelsLikeF);
         console.log(feelsLike);
+        humidity = localStorage.setItem("humidity", weather.current_condition[0].humidity);
+        actualTemp = localStorage.setItem("actualTemp", weather.current_condition[0].temp_F);
+        weatherDesc = localStorage.setItem("weatherDesc", weather.current_condition[0].weatherDesc[0].value);
     });
 
     function addWeatherApi() {
-        let localWeather = localStorage.getItem("weather");
-        const newDiv = document.createElement("div");
-        newDiv.innerHTML = `We recommend you try: ${localWeather}!`;
-        document.getElementById("weatherFig").appendChild(newDiv);
+        let localWeather = localStorage.getItem("actualTemp");
+        let localHumidity = localStorage.getItem("humidity")
+        let localFeelsLike = localStorage.getItem("feelsLike")
+        let localWeatherDesc= localStorage.getItem("weatherDesc")
+        
+        const weatherDiv = document.createElement("div");
+        weatherDiv.innerHTML = `It is ${localWeatherDesc} today<br>
+        Temp: ${localWeather}<br>
+        Feels Like: ${localFeelsLike}<br>
+        Humidity: ${localHumidity}<br>
+        
+
+
+        
+        `;
+        document.getElementById("weatherFig").appendChild(weatherDiv);
     }
+
+    addWeatherApi();
